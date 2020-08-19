5eb038d6ebada638bf14cceda3919cc883208c83
unknown <hgalassi17@gmail.com>
2020-08-19 19:40:11 -0300

    Commit inicial

---
+++ src/main/java/br/com/springexample/hateoasspring/HateoasSpringApplication.java
@@ -0,0 +1,13 @@
+package br.com.springexample.hateoasspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HateoasSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HateoasSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/springexample/hateoasspring/HateoasSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.springexample.hateoasspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HateoasSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
